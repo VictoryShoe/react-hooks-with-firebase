7d5c6149af0201f2b04782049aed009577968315
Victor Shi <victoryshoe@gmail.com>
2020-03-22 20:30:03 -0400

    Change state hook example to liking messages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React, { useState } from 'react';
 
 function App() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState('Funny joke');
+  const [likes, setLikes] = useState(0);
+  const [liked, setLiked] = useState(false);
+
+  function handleClick() {
+    setLikes(likes + 1);
+    setLiked(true);
+  }
+
+  const voteButton = (
+    <button onClick={handleClick}>
+      I like this
+    </button>
+  );
+
+  const likesResult = (
+    <div>
+      <p>
+        {likes} likes
+      </p>
+      <button onClick={() => setLiked(false)}>
+        Like again
+      </button>
+    </div>
+  );
 
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      <p>{message}</p>
+      {liked ? likesResult : voteButton }
     </div>
   );
 }
