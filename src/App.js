252a67e23e0267a538fb1caae61c7c0c50a1b145
Victor Shi <victoryshoe@gmail.com>
2020-03-22 22:15:46 -0400

    Add jokes to Firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
 import React, { useState } from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+const jokes = db.collection('jokes');
+
+let setJoke1 = jokes.doc('0').set({
+  text: 'I ate a clock yesterday, it was very time-consuming.'
+});
+let setJoke2 = jokes.doc('1').set({
+  text: 'Have you played the updated kids’ game? I Spy With My Little Eye... Phone.'
+});
+let setJoke3 = jokes.doc('2').set({
+  text: 'A perfectionist walked into a bar... apparently, the bar wasn’t set high enough.'
+});
 
 function App() {
   const [message, setMessage] = useState('Funny joke');
